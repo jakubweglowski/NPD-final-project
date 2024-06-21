52bcfdc3bca47aed10dede0039f8fd29d306bd7e
jakubweglowski <jw430620@students.mimuw.edu.pl>
2024-06-21 16:11:16 +0200

    Input of 'load_data.py' can now be a list

--- load_data.py
+++ load_data.py
@@ -1,17 +1,22 @@
 import requests
 import pandas as pd
 
-def download_file(url: str, filename: str, output_path: str) -> None:
+def download_file(url: str, filenames: list[str], output_path: str) -> None:
     
     if url[-1] != "/":
         url += "/"
+    
+    if output_path[-1] != "/":
+        output_path += "/"
         
-    assert filename[-7:] == ".tsv.gz", "File should have a '.tsv.gz' extension specified."
-        
-    response = requests.get(url+filename, stream=True)
-    if response.status_code == 200:
-        with open(output_path, "wb") as f:
-            f.write(response.raw.read())
+    for filename in filenames:
+        assert filename[-7:] == ".tsv.gz", "File should have a '.tsv.gz' extension specified."
+            
+        response = requests.get(url+filename, stream=True)
+        if response.status_code == 200:
+            with open(output_path+filename, "wb") as f:
+                f.write(response.raw.read())
+                
 
 def load_to_dataframe(path_to_file: str) -> pd.DataFrame:
     return pd.read_csv(path_to_file,
